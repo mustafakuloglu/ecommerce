cb0d84d87a350bed40d5a25987dff04ff7613751
mustafakuloglu <mustafakuloglu@outlook.com>
2016-07-28 17:41:39 +0300

    Drawer ve tab base activity de birleştirildi.

--- app/src/main/java/gm/com/ecommerce/activities/MainActivity.java
+++ app/src/main/java/gm/com/ecommerce/activities/MainActivity.java
@@ -1,17 +1,18 @@
 package gm.com.ecommerce.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import gm.com.ecommerce.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DrawerAcivity drawerAcivity = new DrawerAcivity();
-        drawerAcivity.drawerCreate(this);
+        TabActivity tabActivity=new TabActivity();
+        tabCreate();
+        tabActivity.drawerCreate(this);
+
     }
 }
--- app/src/main/java/gm/com/ecommerce/activities/TabActivity.java
+++ app/src/main/java/gm/com/ecommerce/activities/TabActivity.java
@@ -1,5 +1,6 @@
 package gm.com.ecommerce.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,19 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +34,15 @@ public class TabActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+    }
+    public void tabCreate(){
         setContentView(R.layout.activity_tab);
         viewPager=(ViewPager)findViewById(R.id.viewpager);
 
@@ -32,8 +50,6 @@ public class TabActivity extends AppCompatActivity {
 
         tabLayout=(TabLayout)findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-
-
     }
     private void setupViewPager(View viewpager)
     {
@@ -74,5 +90,44 @@ public class TabActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitlelist.get(position);
         }
+    }
+    public void drawerCreate(Activity activity) {
+
+        toolbar=(Toolbar) activity.findViewById(R.id.tool_bar);
+        new DrawerBuilder().withActivity(activity).build();
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.app_name);
+        SecondaryDrawerItem item2 = (SecondaryDrawerItem) new SecondaryDrawerItem().withIdentifier(2).withName(R.string.app_name);
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(activity)
+                .withHeaderBackground(R.drawable.material_drawer_shadow_top)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(R.drawable.material_drawer_circle_mask)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        return false;
+                    }
+
+
+                })
+                .build();
+        Drawer result = new DrawerBuilder()
+                .withActivity(activity)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName(R.string.app_name)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        return false;}
+                })
+                .build();
     }
 }
