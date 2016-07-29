ee3a5341daabdcdad04b05ab014c35f5eb0bf395
mustafakuloglu <mustafakuloglu@outlook.com>
2016-07-29 22:30:34 +0300

    Activity J tamamlandı

--- app/src/main/java/gm/com/ecommerce/activities/Activity15.java
+++ app/src/main/java/gm/com/ecommerce/activities/Activity15.java
@@ -2,16 +2,24 @@ package gm.com.ecommerce.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import gm.com.ecommerce.R;
 
 public class Activity15 extends AppCompatActivity {
 TabActivity tabActivity;
+    ImageView shop,search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_15);
         tabActivity=new TabActivity();
+        shop=(ImageView)findViewById(R.id.shop);
+        search=(ImageView)findViewById(R.id.search);
         tabActivity.drawerCreate(this,"Form");
+        tabActivity.toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        shop.setVisibility(View.GONE);
+        search.setVisibility(View.GONE);
     }
 }
